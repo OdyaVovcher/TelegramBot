ad67f32b740a0a9b6a6b7d7a651695a3db4caf6f
Odya_Vovcher <bloodoof1234@gmail.com>
2021-11-03 19:32:38 +0600

    Load Project

---
+++ MainApp/main.py
@@ -0,0 +1,51 @@
+import telebot
+from news_parser import parse
+from telebot import types
+
+
+token = "2024403137:AAG-ZXMvorH6nMrApa0JlELgdF-fERtU15g"
+bot = telebot.TeleBot(token)
+
+
+last_news = ""
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message,
+                 f"Привет {message.from_user.first_name}. \nЧтобы узнать последние новости набери 'Новости'")
+
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message, f"Чтобы узнать последние новости набери 'Новости'")
+
+
+@bot.message_handler(func=lambda message: True)
+def send_news(message):
+    if message.text.upper() == "НОВОСТИ":
+        global last_news
+        content = parse()
+        if last_news != content["title"]:
+            keyboard = types.InlineKeyboardMarkup()
+            key_read = types.InlineKeyboardButton(text="Читать статью", callback_data="read")
+            keyboard.add(key_read)
+            bot.send_photo(message.from_user.id,
+                           photo=content['pict'],
+                           caption=f"{content['date']}\n{content['title']}",
+                           reply_markup=keyboard)
+            last_news = content["title"]
+        else:
+            bot.send_message(message.from_user.id, text="Других новостей пока нет")
+    else:
+        bot.reply_to(message, text=message.text)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == "read":
+        content_body = parse(body=True)
+        bot.send_message(call.message.chat.id,  text=f"{content_body}")
+
+
+bot.infinity_polling()
---
+++ MainApp/news_parser.py
@@ -0,0 +1,50 @@
+import requests
+import unicodedata
+from bs4 import BeautifulSoup
+
+URL_start = "https://lenta.ru/"
+url_body = ""
+
+
+def get_connect(url, params=None):
+    r = requests.get(url, params=params)
+    return r
+
+
+def get_news_title(html):
+    global URL_start
+    global url_body
+    soup = BeautifulSoup(html, "html.parser")
+    item = soup.find("div", class_="first-item")
+    date = item.find("time").get("datetime")
+    pict = item.find("img").get("src")
+    title = item.find_all("a")[1].text[5:]
+    title = unicodedata.normalize("NFKD", title)
+    url_body = URL_start + item.find_all("a")[1].get("href")
+    news = {'date': date,
+            'pict': pict,
+            'title': title}
+    return news
+
+
+def get_news_body(html):
+    soup = BeautifulSoup(html, "html.parser")
+    item = soup.find("p")
+    return item.text
+
+
+def parse(body=False):
+    html = get_connect(URL_start)
+    if html.status_code == 200:
+        news = get_news_title(html.text)
+    else:
+        return "error 404"
+    if body:
+        html = get_connect(url_body)
+        if html.status_code == 200:
+            return get_news_body(html.text)
+    return news
+
+
+if __name__ == "__main__":
+    parse(body=True)
